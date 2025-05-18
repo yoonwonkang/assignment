e647a2bc618bb1bbcb88055cf001b58a50063d95
yoonwon.kang <yoonwon.kang@gmail.com>
2025-05-18 18:57:40 -0300

    define each appliance mode enum

---
+++ src/main/java/com/example/demo/enums/ApplianceType.java
@@ -0,0 +1,9 @@
+package com.example.demo.enums;
+
+/**
+ * When We support more IOT appliance need to add
+ */
+public enum ApplianceType {
+  FAN, LIGHT, AIR_CONDITIONER
+
+}
--- src/main/java/com/example/demo/model/applicance/Appliance.java
+++ src/main/java/com/example/demo/model/applicance/Appliance.java
@@ -1,27 +1,24 @@
 package com.example.demo.model.applicance;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
 
 /**
- * appliance common part
- * appliance can turn off
- * appliance has id, name
- * if wanna appliance more flexible can split function(interface), column class
- * thinking about status need to be a common column?
- * ?? why need to split each device? is that helpful?
+ * every IOT device is appliance
+ * appliance has switchable and mode controllable
  */
-@MappedSuperclass
-public abstract class Appliance {
+@Entity
+public class Appliance {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(length = 50)
   private String name;
+  private String type;
 
-  public abstract void turnOff();
+  private String mode;
+  
+  
 }
