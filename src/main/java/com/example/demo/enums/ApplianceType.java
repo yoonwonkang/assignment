b063132a3293ef6f7f6078ad324d3e8e6501acd8
yoonwon.kang <yoonwon.kang@gmail.com>
2025-05-18 19:09:01 -0300

    ApplianceType has their own mode enum

--- src/main/java/com/example/demo/enums/ApplianceType.java
+++ src/main/java/com/example/demo/enums/ApplianceType.java
@@ -4,6 +4,24 @@ package com.example.demo.enums;
  * When We support more IOT appliance need to add
  */
 public enum ApplianceType {
-  FAN, LIGHT, AIR_CONDITIONER
+  FAN(FanMode.class), 
+  LIGHT(LightMode.class), 
+  AIR_CONDITIONER(AirConditionerMode.class);
 
+  private final Class<? extends ApplianceMode> modeClass;
+
+  ApplianceType(Class<? extends ApplianceMode> modeClass) {
+    this.modeClass = modeClass;
+  }
+
+  public ApplianceMode parseMode(String name) {
+    for (ApplianceMode mode : modeClass.getEnumConstants()) {
+        if (mode.name().equalsIgnoreCase(name)) return mode;
+    }
+    throw new IllegalArgumentException("Invalid mode " + name + " for type " + this.name());
+}
+
+  public Class<? extends ApplianceMode> getModeClass() {
+    return modeClass;
+  }
 }
