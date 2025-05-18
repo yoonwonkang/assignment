51e5ba0f039a75db8d0b65e9536065348acd8562
yoonwon.kang <yoonwon.kang@gmail.com>
2025-05-18 19:16:39 -0300

    apply appliance.mode, appliance.type

--- src/main/java/com/example/demo/model/applicance/Appliance.java
+++ src/main/java/com/example/demo/model/applicance/Appliance.java
@@ -1,9 +1,18 @@
 package com.example.demo.model.applicance;
 
+import com.example.demo.enums.ApplianceMode;
+import com.example.demo.enums.ApplianceType;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Transient;
 
 /**
  * every IOT device is appliance
@@ -16,9 +25,35 @@ public class Appliance {
   private Long id;
 
   private String name;
-  private String type;
 
-  private String mode;
-  
-  
+  @Enumerated(EnumType.STRING)
+  private ApplianceType type;
+
+  private String modeCode;
+
+  @Transient
+  private ApplianceMode mode;
+
+  @PostLoad
+  public void loadMode() {
+    if (type != null && modeCode != null) {
+      this.mode = type.parseMode(modeCode);
+    }
+  }
+
+  @PrePersist
+  @PreUpdate
+  public void persistMode() {
+    if (mode != null) this.modeCode = mode.name();
+    else new NullPointerException("mode is required value");
+  }
+
+  public void setMode(ApplianceMode mode) {
+    this.mode = mode;
+    this.modeCode = mode.name();
+  }
+
+  public ApplianceMode getMode() {
+    return this.mode;
+  }
 }
