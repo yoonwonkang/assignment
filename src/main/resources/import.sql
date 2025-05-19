2af8593d9fcf2732a2d93d6efeb777a2346c06f1
yoonwon.kang <yoonwon.kang@gmail.com>
2025-05-19 16:17:50 -0300

    add import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO appliance (id, type, mode_code) VALUES (1, 'LIGHT', 'ON');
+INSERT INTO appliance (id, type, mode_code) VALUES (2, 'FAN', 'SPEED2');
+INSERT INTO appliance (id, type, mode_code) VALUES (3, 'AIR_CONDITIONER', 'COOL');
